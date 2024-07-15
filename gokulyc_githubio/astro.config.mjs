e43f93f0db6f70195f815a90b7fc786fe65c42a8
Gokul Y <gokulyc@gmail.com>
2024-07-15 12:10:29 +0530

    modified site value

--- gokulyc_githubio/astro.config.mjs
+++ gokulyc_githubio/astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://gokulyc.github.io',
+	site: 'https://github.gokulyc.in',
 	integrations: [mdx(), sitemap()],
 });
