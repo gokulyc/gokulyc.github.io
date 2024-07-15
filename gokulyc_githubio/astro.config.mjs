3f59373ec26d8e67c974bdb17950c2a7d1fc7c19
Gokul Y <gokulyc@gmail.com>
2024-07-15 18:36:02 +0530

    added react to astro project

--- gokulyc_githubio/astro.config.mjs
+++ gokulyc_githubio/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://github.gokulyc.in',
-	integrations: [mdx(), sitemap()],
+  site: 'https://github.gokulyc.in',
+  integrations: [mdx(), sitemap(), react()]
 });
